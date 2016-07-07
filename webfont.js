035352118d34128bcc40e814d3f0c650ea2d4f4a
Max Ivanov <maximiv@gmail.com>
2016-07-07 13:02:37 +0300

    Split js&css from index.html

---
+++ webfont.js
@@ -0,0 +1,16 @@
+(function (root) {
+    'use strict';
+
+    root.WebFontConfig = {
+        google: {families: ['Roboto:300:latin']}
+    };
+
+    (function () {
+        var wf = document.createElement('script');
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    }());
+}(window));
